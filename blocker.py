9be309474169da01f79b31be715694332a320d1b
ELLYSA <ellyky2121@gmail.com>
2020-08-11 20:37:42 +0800

    updated

--- blocker.py
+++ blocker.py
@@ -1,5 +1,5 @@
 import time
-
+from datetime import datetime as dat
 host_path = "C:\Windows\System32\drivers\etc"
 # location of the driver that disables the website
 redirect = '127.0.0.1'
@@ -8,6 +8,11 @@ website_block = ['www.facebook.com', 'facebook.com', 'www.twitter.com']
 
 while True:
     #check the time that will condition the block
-
-    print(1)
-    time.sleep(5)
+    year = dat.now().year
+    month = dat.now().month
+    day = dat.now().day
+    if dat(year,month,day,8) < dat.now() < dat(year,month,day,16):
+        print("Working Hours...")
+    else:
+        print(dat.now())
+        time.sleep(5)
